08b86a1654e25b643cf62d82809917850b70a026
joshgit <jschermann91@gmail.com>
2022-02-25 09:32:00 -0600

    header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Navigation />
     </>
   );
