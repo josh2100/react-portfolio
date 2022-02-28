45babd6d8edd7b821dc4edf6289b19419e9f408f
joshgit <jschermann91@gmail.com>
2022-02-28 16:39:37 -0600

    resume link added

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,16 @@
-function Resume () {
-    return (
-        <>
-        <h2>Resume</h2>
-        <p>Resume here</p>
-        </>
-    )
+function Resume() {
+  return (
+    <>
+      <h2>Resume</h2>
+      <a
+        href="https://docs.google.com/document/d/1QXq_G4lqDAAFZttCU_xLjmseYj-aOZrtnsoHBH3E86o/edit?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click here to download{" "}
+      </a>
+    </>
+  );
 }
 
 export default Resume;
