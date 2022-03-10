7428752b857034146efc098c6c6ce84857d352f9
joshgit <jschermann91@gmail.com>
2022-03-09 18:38:26 -0600

    columns shrunk

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,7 +6,7 @@ function Project({ project }) {
 
   return (
     //col-11 col-sm-4 col-md-5 col-lg col-xl
-    <article className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 mb-4 ms-1 card">
+    <article className="col-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-4 ms-1 card">
       <h2 className="card-header d-flex justify-content-center">{title}</h2>
       <img
         src={require(`../../assets/${imgSrc}.png`)}
