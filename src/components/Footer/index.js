febcef600f747d8678c449d4bb96e9a75bfb2688
joshgit <jschermann91@gmail.com>
2022-03-10 16:40:04 -0600

    hover buttons, footer simplified

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,13 @@
-// import useState from "react";
-
 function Footer() {
   return (
-    <footer className="mt-auto">
-      <button className="col-4">
+    <footer className="mt-auto row">
+      <span className="col-2 offset-2">
         <a href="https://github.com/josh2100" target="_blank" rel="noreferrer">
           GitHub
         </a>
-      </button>
+      </span>
 
-      <button className="col-4">
+      <span className="col-2 offset-1">
         <a
           href="https://www.linkedin.com/in/joshua-schermann-91621734/"
           target="_blank"
@@ -17,9 +15,9 @@ function Footer() {
         >
           LinkedIn
         </a>
-      </button>
+      </span>
 
-      <button className="col-4">
+      <span className="col-2 offset-1">
         <a
           href="https://stackoverflow.com/users/18326954/joshua-s"
           target="_blank"
@@ -27,7 +25,7 @@ function Footer() {
         >
           Stackoverflow
         </a>
-      </button>
+      </span>
     </footer>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,7 +7,7 @@ function Navigation(props) {
     <nav className="row">
         {options.map((selection) => (
           <span
-            className="col-3"
+            className="col-3 pageLinks"
             key={selection.title}
             onClick={() => setSelectedPage(selection)}
           >
