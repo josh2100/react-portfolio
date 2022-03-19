e817bde49182c162aa92489847e389f3512231b2
joshgit <jschermann91@gmail.com>
2022-03-19 07:00:42 -0500

    skills cards

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,4 +1,4 @@
-import resumePdf from "../../assets/resume.pdf"
+import resumePdf from "../../assets/resume.pdf";
 
 function Resume() {
   return (
@@ -14,52 +14,69 @@ function Resume() {
         <button> View resume</button>
       </a>
 
-      <a href={resumePdf} download="resume.pdf" className="col-2 d-flex justify-content-center m-3">
+      <a
+        href={resumePdf}
+        download="resume.pdf"
+        className="col-2 d-flex justify-content-center m-3"
+      >
         <button> Download Resume PDF</button>
       </a>
 
       <h3 className="col-12 d-flex justify-content-center">Skills</h3>
-      <h4 className="col-12 d-flex justify-content-center">Front End</h4>
 
-      <ul>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>jQuery</li>
-        <li>Handlebars.js</li>
-        <li>HTML5</li>
-        <li>CSS</li>
-        <li>Flexbox</li>
-        <li>Bootstrap</li>
-        <li>Responsive Design</li>
-      </ul>
+      {/* Left side */}
+      <div className="col-6 card">
+        <h4 className="col-12 d-flex justify-content-center">Front End</h4>
+        <ul className="col-12">
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>jQuery</li>
+          <li>Handlebars.js</li>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>Flexbox</li>
+          <li>Bootstrap</li>
+          <li>Responsive Design</li>
+        </ul>
+      </div>
 
-      <h4 className="col-12 d-flex justify-content-center">Back End</h4>
-      <ul>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>RESTful APIs</li>
-        <li>Model View Controller</li>
-        <li>MongoDB</li>
-        <li>Mongoose</li>
-        <li>MySQL</li>
-        <li>Sequelize</li>
-        <li>GraphQL</li>
-        <li>Apollo</li>
-        <li>Object Oriented Programming</li>
-        <li>Object Relational Mapping</li>
-      </ul>
+      {/* Right side */}
+      <div className="col-6 card">
+        <h4 className="col-12 d-flex justify-content-center">Back End</h4>
+        <ul className="col-12">
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>RESTful APIs</li>
+          <li>Model View Controller</li>
+          <li>MongoDB</li>
+          <li>Mongoose</li>
+          <li>MySQL</li>
+          <li>Sequelize</li>
+          <li>GraphQL</li>
+          <li>Apollo</li>
+          <li>Object Oriented Programming</li>
+          <li>Object Relational Mapping</li>
+        </ul>
+      </div>
 
-      <h4 className="col-12 d-flex justify-content-center">Testing</h4>
-      <ul>
-        <li>Jest</li>
-        <li>Mocha</li>
-      </ul>
+      {/* Left side */}
+      <div className="col-6 card">
+        <h4 className="col-12 d-flex justify-content-center">Testing</h4>
+        <ul className="col-12">
+          <li>Jest</li>
+          <li>Mocha</li>
+        </ul>
+      </div>
 
-      <h4 className="col-12 d-flex justify-content-center">Collaboration</h4>
-      <ul>
-        <li>Agile</li>
-        <li>GitHub</li>
-      </ul>
+      {/* Right side */}
+      <div className="col-6 card">
+        <h4 className="col-12 d-flex justify-content-center">Collaboration</h4>
+        <ul>
+          <li>Agile</li>
+          <li>GitHub</li>
+          <li>GitLab</li>
+        </ul>
+      </div>
     </section>
   );
 }
