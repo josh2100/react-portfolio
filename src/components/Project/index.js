3f8e78f05d1324419189f657fbd401664ab93c6d
joshgit <jschermann91@gmail.com>
2022-04-25 10:05:51 -0500

    shadows added

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,7 +6,7 @@ function Project({ project }) {
 
   return (
     //col-11 col-sm-4 col-md-5 col-lg col-xl
-    <article className="col-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-4 ms-1 card g-0">
+    <article className="col-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-4 ms-1 card shadow-lg g-0">
       <h2 className="d-flex justify-content-center">{title}</h2>
       <img
         src={require(`../../assets/${imgSrc}.png`)}
@@ -17,10 +17,10 @@ function Project({ project }) {
       <p className="m-2">{body}</p>
       <section className="d-flex justify-content-around m-2 mt-auto">
         <a href={deployed} target="_blank" rel="noreferrer" className="">
-          <button>App</button>
+          <button className="shadow ps-3 pe-3">App</button>
         </a>
         <a href={repo} target="_blank" rel="noreferrer">
-          <button>GitHub</button>
+          <button className="shadow ps-3 pe-3">GitHub</button>
         </a>
       </section>
     </article>
